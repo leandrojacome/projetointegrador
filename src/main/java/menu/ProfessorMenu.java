abdbf76a9a59b0acbd2ff48e9e9e1d87a73fbad3
NickBirrer <64037709+NickBirrer@users.noreply.github.com>
2021-11-22 21:27:15 -0300

    Create ProfessorMenu.java

---
+++ src/main/java/menu/ProfessorMenu.java
@@ -0,0 +1,44 @@
+package menu;
+
+import dominio.Professor;
+import dominio.PessoaFisica;
+import form.ProfessorForm;
+import infra.Arquivo;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void mostrarMenu() {
+        Scanner entrada = new Scanner(System.in);
+        String escolha, cont = "s";
+        while (cont.equalsIgnoreCase("s")) {
+            System.out.println("\t\t Gestão de Professor \n\n");
+
+            System.out.println("1 ===> Listar ");
+            System.out.println("2 ===> Incluir ");
+            System.out.println("3 ===> Visualizar ");
+            System.out.println("4 ===> Alterar ");
+            System.out.println("5 ===> Excluir ");
+
+            System.out.print("\n\n");
+            System.out.println("Escolha uma opção: ");
+            escolha = entrada.nextLine();
+
+            switch (escolha) {
+                case "2":
+                    Professor professor = ProfessorForm.incluir();
+                    Arquivo<PessoaFisica> pessoaFisicaArquivo = new Arquivo<>(professor.getPessoaFisica());
+                    Arquivo<Professor> alunoArquivo = new Arquivo<>(professor);
+                    try {
+                        pessoaFisicaArquivo.salvar();
+                        professorArquivo.salvar();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        }
+    }
+}
